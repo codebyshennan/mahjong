fda03100cd641275e8059e34424d4857a0c22e17
Shen Nan, Wong <wongshennan@gmail.com>
2026-04-30 09:46:00 +0700

    checkpoint: edit /Users/wongshennan/Documents/personal/learning/bootcamps/rocketacademy/mahjong/public/js/rooms/interstitial/interstitial.js

--- public/js/rooms/interstitial/interstitial.js
+++ public/js/rooms/interstitial/interstitial.js
@@ -18,9 +18,11 @@ const firebase = initializeApp(firebaseConfig)
 const auth = getAuth()
 const rtdb = getDatabase(firebase)
 const fsdb = getFirestore(firebase)
-connectAuthEmulator(auth, "http://localhost:9099")
-connectDatabaseEmulator(rtdb, "localhost", 9000)
-connectFirestoreEmulator(fsdb, "localhost", 8080)
+if (location.hostname === 'localhost') {
+  connectAuthEmulator(auth, "http://localhost:9099")
+  connectDatabaseEmulator(rtdb, "localhost", 9000)
+  connectFirestoreEmulator(fsdb, "localhost", 8080)
+}
 
 window.addEventListener('DOMContentLoaded', async ()=> {
 
